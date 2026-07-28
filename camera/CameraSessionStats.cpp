2436ef404ba2ee0dc4916781c0f6a4d1e1dbe8b7
dryerlint <dryerlintie@proton.me>
2026-07-28 12:14:27 -0400

    zooming

--- camera/CameraSessionStats.cpp
+++ camera/CameraSessionStats.cpp
@@ -260,8 +260,12 @@ const int CameraSessionStats::CAMERA_STATE_ACTIVE = 1;
 const int CameraSessionStats::CAMERA_STATE_IDLE = 2;
 const int CameraSessionStats::CAMERA_STATE_CLOSED = 3;
 
+// zooming
+__attribute__((used, visibility("default")))
 const int CameraSessionStats::CAMERA_STATE_EXCEPTION = 4;
+__attribute__((used, visibility("default")))
 const int CameraSessionStats::CAMERA_STATE_SESSION_CONFIGURED = 5;
+__attribute__((used, visibility("default")))
 const int CameraSessionStats::CAMERA_STATE_FIRST_FRAME_ARRIVED = 6;
 
 const int CameraSessionStats::CAMERA_FACING_BACK = 0;
